4101bd4f590ef8b086da37c9f68faebf01abd209
Al-Jhoenil Wahid <aljhoenilw@gmail.com>
2019-08-22 00:09:07 +0800

    add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.listen(8000,function(){
 console.log("server is running")
 })
 
+/* GET REQUEST */
 const mockUserData=[
 {name:'Mark'},
 {name:'Jill'}
@@ -20,4 +23,26 @@ app.get('/users', function(req,res){
 
 app.get('/users/:id',function(req,res){
 console.log(req.params.id)
+})
+
+/* POST REQUEST */
+app.post('/login',function(req,res){
+ 	const username=req.body.username;
+ 	const password=req.body.password;
+ 
+ 	const mockUsername="billyTheKid";
+ 	const mockPassword="superSecret";
+ 
+ 	if (username===mockUsername && password===mockPassword){
+      	res.json({
+      	 	success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+      	})
+ 	} else {
+      	res.json({
+      	 	success: false,
+      	 	message: 'password and username do not match'
+      	})
+ 	}
 })
